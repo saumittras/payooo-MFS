a8a71e6127163f7b1f104112ce2e0126ed637615
saumittras <saumittra.eee@gmail.com>
2024-09-20 00:46:19 +0600

    added all

--- js/addMoney.js
+++ js/addMoney.js
@@ -4,9 +4,20 @@ document
     event.preventDefault();
     const addMoney = getInputFieldValueById("input-add-money");
     const pinNumber = getInputFieldValueById("pin-number");
+    if (isNaN(addMoney)) {
+      alert("Invalid Amount");
+      return;
+    }
+
     if (pinNumber === 1234) {
       const newBalance = getTextFieldValueById("account-balance") + addMoney;
       document.getElementById("account-balance").innerText = newBalance;
+
+      // add to transaction history
+      const p = document.createElement("p");
+      p.innerText = `Added: ${addMoney} Tk. New Blance ${newBalance}`;
+      console.log(p);
+      document.getElementById("transaction-section").appendChild(p);
     } else {
       alert("Wrong Pin Number");
     }
--- js/cashOut.js
+++ js/cashOut.js
@@ -4,10 +4,26 @@ document
     event.preventDefault();
     const cashOutAmount = getInputFieldValueById("input-cash-out-amount");
     const pinNumber = getInputFieldValueById("pin-number-cash-out");
+    if (isNaN(cashOutAmount)) {
+      alert("Invalid Amount");
+      return;
+    }
     if (pinNumber === 1234) {
-      const newBalance =
-        getTextFieldValueById("account-balance") - cashOutAmount;
+      const blance = getTextFieldValueById("account-balance");
+      if (blance < cashOutAmount) {
+        alert("Insufficent Blance");
+        return;
+      }
+      const newBalance = blance - cashOutAmount;
       document.getElementById("account-balance").innerText = newBalance;
+      // add transation history
+      const div = document.createElement("div");
+      div.classList.add("bg-yellow-300");
+      div.innerHTML = `
+       <h4 class="text-2xl font-bold">Cash Out</h4>
+       <p>${cashOutAmount} withdraw. New Blance ${newBalance}</p>
+       `;
+      document.getElementById("transaction-container").appendChild(div);
     } else {
       alert("Wrong Pin Number");
     }
